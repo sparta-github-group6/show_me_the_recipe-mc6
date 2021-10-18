dd9b7b4be35c481f8d527fedbc365c1ccdb73b98
jiwoong Moon <mjw92@kakao.com>
2021-10-18 12:07:58 +0900

    second commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, jsonify, request
+from pymongo import MongoClient
+
+app = Flask(__name__)
+# client = MongoClient('', )
+# db = client.dbreceipe
+
+#HTML 화면 표시
+@app.route('/')
+def init():
+    return render_template('index.html')
+
+#요리 레시피 요청
+
+
+#추천 요리 표시
