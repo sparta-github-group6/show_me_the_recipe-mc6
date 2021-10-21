a5e9954774546bef86ecba1da9605bc3785cfd6c
jwm92 <mjw92@kakao.com>
2021-10-21 22:50:59 +0900

    woong second commit

--- crawling_menus.py
+++ crawling_menus.py
@@ -1,13 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 
-headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-data = requests.get('https://terms.naver.com/list.naver?cid=42701&categoryId=62872&so=st4.asc',headers=headers)
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get('https://terms.naver.com/list.naver?cid=42701&categoryId=62872&so=st4.asc', headers=headers)
 
 soup = BeautifulSoup(data.text, 'html.parser')
 
-content = soup.find("li", {"class":"contents_sub active"})
-menus = content.findAll("a", {"class":None})
+content = soup.find("li", {"class": "contents_sub active"})
+menus = content.findAll("a", {"class": None})
 
 for menu in menus:
     name = menu.text
