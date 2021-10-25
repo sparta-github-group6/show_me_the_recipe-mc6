423f252195f031a781f4d5d9a0af8f2032f21d9c
kjh8673a <kjh8673a@naver.com>
2021-10-26 00:27:48 +0900

    like

--- static/main.js
+++ static/main.js
@@ -34,32 +34,38 @@ function recommend() {
             for (let i = 0; i < recipes.length; i++) {
                 let ing = recipes[i]['ingredients']
                 let name = recipes[i]['name']
+                let like = recipes[i]['like']
 
                 let temp_html = `<a href="/recipe" onclick="search_recipe('${name}')">
                                     <div class="recipes_all_about">
                 
                                         <div class="recipes_img">
-                                            <img src="../static/recipe-image/${name}.png" class="list-img-cook" alt="요리 이미지">
+                                            <a>
+                                                <img src="../static/recipe-image/${name}.png" class="list-img-cook" alt="요리 이미지">
+                                            </a>
                                         </div>
                 
                                         <div class="recipes_information">
                                             <div class="recipes_title" id="index${i}">
-                                                <h3>레시피 제목: ${name} </h3>
+                                                <h3> ${name} </h3>
                                             </div>
                 
                                             <div class="recipes_desc">
-                                                <h4>레시피 재료</h4>
-                                 
                                                 <p>
-                                                    ${ing}
+                                                    재료 : ${ing}
                                                 </p>
                                             </div>
                 
-                                            <div class="recipes_like"><i class="far fa-thumbs-up"></i> 10 </div>
+                                            <div class="recipes_like">
+                                            <footer class="card-footer">
+                                                    <a href="#" onclick="likeStar('${name}')" class="card-footer-item has-text-info">
+                                                    ${like}
+                                                        <span class="icon">
+                                              <i class="fas fa-thumbs-up"></i>
+                                                    </a>
                                         </div>
-                
                                     </div>
-                                </a>`
+                                            </a>`
 
                 $('.recipe-box').append(temp_html)
             }
@@ -67,6 +73,17 @@ function recommend() {
     })
 }
 
+function likeStar(name) {
+                $.ajax({
+                    type: 'POST',
+                    url: '/recipe/like',
+                    data: {name_give:name},
+                    success: function (response) {
+                        alert(response['msg']);
+                        window.location.reload()
+                    }
+                });
+            }
 
 function search_recipe(name){
     $.ajax({
