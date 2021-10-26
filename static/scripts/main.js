fa8fc818faad6bef578181b70d2c3aa97ca22060
donghyeon23 <gjbmt123@gmail.com>
2021-10-26 21:16:58 +0900

    list goood

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -4,33 +4,30 @@ function get_Favorite(){
 }
 
 // 재료 담을 리스트 생성
-let ing_list = [];
+var ing_list = [];
 
 // 선택 버튼 누르면 동작하는 함수
 function add_ing_list() {
     let item = document.getElementById("ingredient").value;
     console.log(item)
-    ing_list.push(item+"_");
+    ing_list.push(item);
 
     let selected_box = $("#selected");
     selected_box.show();
 
     let item_list = $("#selected-item-list");
     item_list.append("<li>"+item+"</li>");
-    temp(item_list);
 }
 
 
-function temp(item_list) {
-    console.log(item_list);
-}
 
 function to_recommend() {
     var ingredient = $('#ingredient').val();
     $.ajax({
         type: "POST",
         url: "/recommend/search",
-        data: {ing_give: ingredient},
+        data: {ing_list},
+        traditional: true,
         success: function (response) {
             console.log(response)
         }
