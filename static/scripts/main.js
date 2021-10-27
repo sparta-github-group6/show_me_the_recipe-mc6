39a50f1aa31c5e93328732c5dcb8e15a35155ab1
kjh8673a <kjh8673a@naver.com>
2021-10-27 22:17:52 +0900

    connect to recipe

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -14,23 +14,22 @@ function get_Favorite() {
                 let name = favorite_list[i]['name']
                 let like = favorite_list[i]['like']
 
-                let temp_html = `
+                let temp_html = `<a href="/recipe" onclick="index_search_recipe('${name}')">
                                     <div>
                                         <div class="favorite-card-cook">
                                             <div>
                                                 <img src="../static/recipe-image/${name}.png" class="favorite-dishes" alt="인기 메뉴">
                                             </div>
-                                        <div class="rank-card-cook-title">
-                                            <h3>${name}</h3>
-                                        </div>
+                                            <div class="rank-card-cook-title">
+                                                <h3>${name}</h3>
+                                            </div>
                                     
-                                        <div class="favorite-card-cook-like">
-                                            <h3><i class="fas fa-thumbs-up"></i>  ${like}</h3>
-                                        
+                                            <div class="favorite-card-cook-like">
+                                                <h3><i class="fas fa-thumbs-up"></i>  ${like}</h3>
+                                            </div>
                                         </div>
-                    
                                     </div>
-                                `
+                                </a>`
 
                 $('#favorite-dishes').append(temp_html)
             }
@@ -77,5 +76,13 @@ function to_recommend() {
     location.href = "/recommend";
 }
 
-
+function index_search_recipe(name){
+    $.ajax({
+        type: "POST",
+        url: "/recommend/search2",
+        data: {name_give: name},
+        success: function (response) {
+            }
+    })
+}
 
