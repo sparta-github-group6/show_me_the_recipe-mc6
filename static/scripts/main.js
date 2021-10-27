fbe16aceee3be3bf59c6f79cfb4bc8fef0ac3584
donghyeon23 <gjbmt123@gmail.com>
2021-10-28 02:00:20 +0900

    team commit 5

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,16 +1,16 @@
-       $(document).ready(function () {
-            get_Favorite();
-        });
+$(document).ready(function () {
+    get_Favorite();
+});
 
 //좋아요 상위 4개 요리 가져오기
 function get_Favorite() {
     $.ajax({
-        type:'GET',
+        type: 'GET',
         url: '/rank/favorite?sample_give=샘플데이터', //레시피 가져올 url  필요
-        data:{},
-        success: function (response){
+        data: {},
+        success: function (response) {
             let favorite_list = response['favorite_Lists']
-            for (let i = 0; i < favorite_list.length; i++ ) {
+            for (let i = 0; i < favorite_list.length; i++) {
                 let name = favorite_list[i]['name']
                 let like = favorite_list[i]['like']
 
@@ -41,38 +41,47 @@ function get_Favorite() {
 
 // 재료 담을 리스트 생성
 let ing_list = [];
+let count = 1;
 
 // 선택 버튼 누르면 동작하는 함수
 function add_ing_list() {
+    let name = $('#ingredient').val();
+    if (name == '') {
+        alert('재료를 입력해주세요.')
+    } else {
+            let item = document.getElementById("ingredient").value;
+            console.log(item)
+            ing_list.push(item);
 
-    let item = document.getElementById("ingredient").value;
-    console.log(item)
-    ing_list.push(item);
+            let selected_box = $("#selected");
+            selected_box.show();
 
-    let selected_box = $("#selected");
-    selected_box.show();
-
-    let item_list = $("#selected-item-list");
+            let item_list = $("#selected-item-list");
 
 
-    // item_list.append("<li class='sel_item'>" + item + "</li>");
+            // item_list.append("<li class='sel_item'>" + item + "</li>");
+            let temp_html = `<li id="sel_item${count}">${item}<button class="btn-main del_item" onclick="delete_item(${count})"><i class="fas fa-times del_item"></i></button></li>`
+            item_list.append(temp_html)
 
-    let temp_html = `<li id="sel_item">${item}<button class="btn-main del_item" onclick="delete_item()"><i class="fas fa-times del_item"></i></button></li>`
-    item_list.append(temp_html)
-
-    $("#ingredient").val("")
-
+            $("#ingredient").val("")
+            count++;
+    }
 }
 
-function delete_list(){
+function delete_list() {
     $("#selected-item-list").empty();
-    ing_list.clear();
+    ing_list = [];
 }
 
-function delete_item(){
-   const item = document.getElementById('sel_item');
-    item.remove();fg
-    ing_list.pop();
+function delete_item(count) {
+    let del = '#sel_item' + String(count)
+    // const ul = document.getElementById('selected-item-list');
+    // const item = document.getElementById('sel_item');
+    // const items = ul.getElementsByTagName('li');
+    let del2 = $(del).text();
+    $(del).remove();
+    const idx = ing_list.indexOf(del2)
+    ing_list.splice(idx, 1)
 }
 
 
@@ -91,13 +100,13 @@ function to_recommend() {
     location.href = "/recommend";
 }
 
-function index_search_recipe(name){
+function index_search_recipe(name) {
     $.ajax({
         type: "POST",
         url: "/recommend/search2",
         data: {name_give: name},
         success: function (response) {
-            }
+        }
     })
 }
 
