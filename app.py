bcdbb470371ddd11481a422c623f69e08834a397
donghyeon23 <gjbmt123@gmail.com>
2021-10-28 21:55:32 +0900

    add server pymongo

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ from flask import Flask, render_template, jsonify, request
 from pymongo import MongoClient
 
 app = Flask(__name__)
-client = MongoClient('localhost', 27017)
+
+client = MongoClient('mongodb://test:test@localhost', 27017)
+# client = MongoClient('localhost', 27017)
 db = client.dbmaking
 
 
