35d49028e3ea621de9153d15e0c30a7fa3161b79
jwm92 <mjw92@kakao.com>
2021-11-01 20:58:46 +0900

    login add

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from pymongo import MongoClient
 
 app = Flask(__name__)
 
-client = MongoClient('mongodb://test:test@localhost', 27017)
-# client = MongoClient('localhost', 27017)
+# client = MongoClient('mongodb://test:test@localhost', 27017)
+client = MongoClient('localhost', 27017)
 db = client.dbmaking
 
 
