bdc48f42cd5f779830c1cba69636f07b43751620
jwm92 <mjw92@kakao.com>
2021-11-08 00:08:46 +0900

    jjinja2

--- static/scripts/rank.js
+++ static/scripts/rank.js
@@ -1,4 +1,4 @@
-$(document).ready(() => {
+$(document).ready(() => {   
     show_rank();
 })
 
--- static/scripts/toggle.js
+++ static/scripts/toggle.js
@@ -1,7 +1,11 @@
 $(function(){
 
-$('.btn_menu').on('click', function(){
+    $("#navigation").load("templates/nav.html");
+
+    $('.btn_menu').on('click', function(){
     $(this).toggleClass('active');
     $('.navigation').toggleClass('active');
+    })
+
 })
-})
+
